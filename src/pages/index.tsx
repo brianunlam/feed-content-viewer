6826446cf1fa69f6b481c2dbe601499cfa4bfea0
Brian <brian.alonso.a@gmail.com>
2020-04-19 14:59:53 -0300

    Test branch protection on github (#1)
    * Test branch protection on github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import css from '../styles/main.scss';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js 👋👋👋👋👋👋</h1>
     <p>
     <div className={css.example}>Hello World!</div>
     </p>
