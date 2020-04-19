6ae4b48c26df4817363e1f6b2dc8318016107785
Brian Alonso <brian.alonso.a@gmail.com>
2020-04-19 13:25:07 -0300

    Add tslint and prettier

--- src/components/ListItem.test.tsx
+++ src/components/ListItem.test.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react'
+import * as React from 'react';
 import { shallow } from 'enzyme';
 import ListItem from './ListItem';
 
 test('List items renders ok', () => {
-  const listItem = shallow(<ListItem data={{ id: 10, name: 'brian'}}/>);
+  const listItem = shallow(<ListItem data={{ id: 10, name: 'brian' }} />);
   expect(listItem.prop('href')).toEqual('/users/[id]');
   expect(listItem.prop('as')).toEqual('/users/10');
   expect(listItem.children().text()).toEqual('10: brian');
