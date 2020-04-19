3eecccfe907853c771d4513f49e9fb60ca5c1014
Brian Alonso <brian.alonso.a@gmail.com>
2020-04-19 14:10:03 -0300

    Add sass support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
-import Link from 'next/link';
 import Layout from '../components/Layout';
+import css from '../styles/main.scss';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+    <div className={css.example}>Hello World!</div>
     </p>
   </Layout>
 );
