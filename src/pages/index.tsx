f95bf0dfc3264517604d2bf04149222931bcc369
Brian <brian.alonso.a@gmail.com>
2020-04-28 02:53:20 -0300

    Use styled components (#5)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import Layout from '../components/Layout';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋👋👋👋👋👋</h1>
+    <Title>Hello Next.js 👋👋👋👋👋👋</Title>
     <p>
-    <div>Hello World without sass!</div>
+      <div>Hello World with styled components!</div>
     </p>
   </Layout>
 );
