19b15735e50ee39cb8032a77a7eea896c83cf4bc
Brian Alonso <brian.alonso.a@gmail.com>
2020-04-28 02:30:57 -0300

    Modify text in order to trigger travis

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋👋👋👋👋👋</h1>
     <p>
-    <div>Hello World!</div>
+    <div>Hello World without sass!</div>
     </p>
   </Layout>
 );
