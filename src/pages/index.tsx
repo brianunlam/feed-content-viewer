c427f88d2ed953a4a41929fd5fed9f0a41d5d52a
Brian <brian.alonso.a@gmail.com>
2020-04-28 02:33:26 -0300

    Remove sass (#4)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import Layout from '../components/Layout';
-import css from '../styles/main.scss';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋👋👋👋👋👋</h1>
     <p>
-    <div className={css.example}>Hello World!</div>
+    <div>Hello World without sass!</div>
     </p>
   </Layout>
 );
